a962f76a2dad663b44a4138d3eb944782e03084c
qu4resm4 <quaresmacont@gmail.com>
2026-03-20 21:00:26 -0300

    ex3 esboço

---
+++ ex3/ft_achievement_tracker.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+#import random
+
+def gen_player_achievements(achievements: set) -> set:
+    achievements = ['Crafting Genius', 'Strategist', 'World Savior',
+                    'Speed Runner', 'Survivor', 'Master Explorer',
+                    'Treasure Hunter', 'Unstoppable', 'First Steps',
+                    'Collector Supreme', 'Untouchable', 'Sharp Mind',
+                    'Boss Slayer']
+    achievements = set(achievements)
+    print(achievements)
